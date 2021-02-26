2261d13d9b54ebebb24e4fdf7c4db92b2b4c5d69
harrik123 <harri.kuittinen@gmail.com>
2021-02-26 23:32:54 +0200

    Style input bar

--- src/components/InputBar/index.tsx
+++ src/components/InputBar/index.tsx
@@ -3,13 +3,32 @@ import styled from "styled-components";
 import { IInputBarProps } from "../../ts/intefaces";
 import Button from "../Button";
 
-const Form = styled.form`
-  display: grid;
-  grid-template-columns: 7fr 2fr;
+const Form = styled.form``;
+
+export const Input = styled.input`
+  appearance: none;
+  margin: 15px 0;
+  border: 1px solid #dddfe2;
+  display: block;
+  width: 100%;
+  box-sizing: border-box;
+  line-height: 1.6;
+  padding: 10px 12px;
+  box-shadow: 0 2px 4px rgb(0 0 0 / 10%), 0 8px 16px rgb(0 0 0 / 10%);
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
 `;
 
 const SubmitButton = styled(Button)`
-  background-color: green;
+  font-size: 1.1em;
+  padding: 0.5em 1em;
+  background-color: #fff;
+  border: 2px solid #000;
+  box-shadow: 3px 3px #ff9908;
+  color: #000;
 `;
 
 function InputBar(props: IInputBarProps) {
@@ -30,14 +49,16 @@ function InputBar(props: IInputBarProps) {
 
   return (
     <Form onSubmit={onFormSubmit}>
-      <input
+      <Input
         placeholder={props.placeholder}
         value={inputText}
         onChange={onInputChange}
       />
-      <SubmitButton type="submit" value="Submit">
-        {props.submitButtonText}
-      </SubmitButton>
+      <ButtonWrapper>
+        <SubmitButton type="submit" value="Submit">
+          {props.submitButtonText.toUpperCase()}
+        </SubmitButton>
+      </ButtonWrapper>
     </Form>
   );
 }
